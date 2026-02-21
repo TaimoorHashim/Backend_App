1376799ec472d19b3963d39eb7deaf5652619968
Taimoor Hashim <hunainhashim906@gmail.com>
2026-02-21 23:00:34 +0500

    "import packages/Use packages"

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 dotenv.config({ path: "./env" });
+
 connectDB();
+try {
+  app.on("error", (err) => {
+    console.log("Error in app:", err);
+  });
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 8000}`);
+  });
+} catch (error) {
+  console.log("Error connecting to database:", error);
+}
